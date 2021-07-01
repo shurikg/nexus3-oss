04fb301a36242619a5314c7be1f3964e6e868f71
shurik <alexge@amdocs.com>
2021-07-01 10:27:46 +0300

    add error manage

--- files/groovy/update_admin_password.groovy
+++ files/groovy/update_admin_password.groovy
@@ -1,5 +1,24 @@
+import groovy.json.JsonOutput
 import groovy.json.JsonSlurper
+
+List<Map<String, String>> actionDetails = []
+@Field Map scriptResults = [changed: false, error: false]
+scriptResults.put('action_details', actionDetails)
+Map<String, String> currentResult = []
 
 parsed_args = new JsonSlurper().parseText(args)
 
-security.securitySystem.changePassword('admin', parsed_args.new_password)
+try {
+    security.securitySystem.changePassword('admin', parsed_args.new_password)
+    currentResult.put('status', 'changed')
+    scriptResults['changed'] = true
+
+} catch (Exception e) {
+    currentResult.put('status', 'error')
+    currentResult.put('error_msg', e.toString())
+    scriptResults['error'] = true
+}
+
+scriptResults['action_details'].add(currentResult)
+
+return JsonOutput.toJson(scriptResults)
