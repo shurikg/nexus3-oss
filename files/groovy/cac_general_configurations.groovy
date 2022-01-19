63183736a7ce0cdf99a051df9fd90cdf75c9168d
Shurik <shurikgefter@gmail.com>
2022-01-19 17:16:58 +0200

    add jetty ssl support

--- files/groovy/cac_general_configurations.groovy
+++ files/groovy/cac_general_configurations.groovy
@@ -32,67 +32,102 @@ def addChange(gitChange, runtimeChange, description) {
 
 def compareNexusVersion(currentVersion, requireVersion) {
     if (currentVersion != requireVersion) {
-        addChange(requireVersion, currentVersion, "nexus version will be change")
+        addChange(requireVersion, currentVersion, 'nexus version will be change')
     }
 }
 
 def compareNexusPort(currentPort, requirePort) {
     if (currentPort.toInteger() != requirePort.toInteger()) {
-        addChange(requirePort, currentPort, "nexus port will be change")
+        addChange(requirePort, currentPort, 'nexus port will be change')
     }
 }
 
 def compareContextPath(currentContextPath, requireContextPath) {
     if (currentContextPath != requireContextPath) {
-        addChange(requireContextPath, currentContextPath, "nexus context path will be change")
+        addChange(requireContextPath, currentContextPath, 'nexus context path will be change')
     }
 }
 
 def compareTimezone(currentTimezone, requireTimezone) {
     if (currentTimezone != requireTimezone) {
-        addChange(requireTimezone, currentTimezone, "nexus timezone will be change")
+        addChange(requireTimezone, currentTimezone, 'nexus timezone will be change')
     }
 }
 
 def compareInstallationDir(currentDir, requireDir) {
     if (currentDir != requireDir) {
-        addChange(requireDir, currentDir, "nexus installation dir will be change")
+        addChange(requireDir, currentDir, 'nexus installation dir will be change')
     }
 }
 
 def compareTempDir(currentDir, requireDir) {
     if (currentDir != requireDir) {
-        addChange(requireDir, currentDir, "nexus temp dir will be change")
+        addChange(requireDir, currentDir, 'nexus temp dir will be change')
     }
 }
 
 def compareUserName(currentUserName, requireUserName) {
     if (currentUserName != requireUserName) {
-        addChange(requireUserName, currentUserName, "nexus user name will be change")
+        addChange(requireUserName, currentUserName, 'nexus user name will be change')
     }
 }
 
 def compareUserHome(currentUserHome, requireUserHome) {
     if (currentUserHome != requireUserHome) {
-        addChange(requireUserHome, currentUserHome, "nexus user home will be change")
+        addChange(requireUserHome, currentUserHome, 'nexus user home will be change')
     }
 }
 
 def compareMinHeapSize(currentSize, requireSize) {
     if (currentSize != requireSize) {
-        addChange(requireSize, currentSize, "nexus min memory heap size will be change")
+        addChange(requireSize, currentSize, 'nexus min memory heap size will be change')
     }
 }
 
 def compareMaxHeapSize(currentSize, requireSize) {
     if (currentSize != requireSize) {
-        addChange(requireSize, currentSize, "nexus max memory heap size will be change")
+        addChange(requireSize, currentSize, 'nexus max memory heap size will be change')
     }
 }
 
 def compareMaxDirectSize(currentSize, requireSize) {
     if (currentSize != requireSize) {
-        addChange(requireSize, currentSize, "nexus max direct memory size will be change")
+        addChange(requireSize, currentSize, 'nexus max direct memory size will be change')
+    }
+}
+
+def compareJettyHttpsEnable(currentJetty, requireJetty) {
+    def currentJettyEnable = currentJetty.contains('jetty-https.xml')
+    if (currentJettyEnable != Boolean.valueOf(requireJetty)) {
+        addChange(requireJetty, currentJettyEnable, 'jetty https configuration will be change')
+    }
+    if (currentJettyEnable && Boolean.valueOf(requireJetty)) {
+        return true
+    }
+    return false
+}
+
+def compareJettyKeyStorePassword(currentPassword, requirePassword) {
+    if (currentPassword != requirePassword) {
+        addChange(requirePassword, currentPassword, 'keystore password will be change')
+    }
+}
+
+def compareJettyKeyManagerPassword(currentPassword, requirePassword) {
+    if (currentPassword != requirePassword) {
+        addChange(requirePassword, currentPassword, 'key manager password will be change')
+    }
+}
+
+def compareJettyTrustStorePassword(currentPassword, requirePassword) {
+    if (currentPassword != requirePassword) {
+        addChange(requirePassword, currentPassword, 'trust store password will be change')
+    }
+}
+
+def compareJettyKeyStoreFile(currentCheckSum, requireCheckSum) {
+    if (currentPassword != requirePassword) {
+        addChange(requirePassword, currentPassword, 'keystore file will be change')
     }
 }
 
@@ -110,23 +145,29 @@ compareNexusVersion(currentDetails['nexus-status']['version'], parsed_args['requ
 compareNexusPort(currentDetails['nexus-properties']['application-port'], parsed_args['require']['nexus_default_port'])
 compareContextPath(currentDetails['nexus-properties']['nexus-context-path'], parsed_args['require']['nexus_default_context_path'])
 compareTimezone(currentDetails['nexus-properties']['user.timezone'], parsed_args['require']['nexus_timezone'])
-compareInstallationDir(currentDetails['nexus-configuration']['installDirectory'], parsed_args['require']['nexus_installation_dir']+'/nexus-'+parsed_args['require']['nexus_version'])
+compareInstallationDir(currentDetails['nexus-configuration']['installDirectory'], parsed_args['require']['nexus_installation_dir'] + '/nexus-' + parsed_args['require']['nexus_version'])
 compareTempDir(currentDetails['nexus-configuration']['temporaryDirectory'], parsed_args['require']['nexus_tmp_dir'])
 compareUserName(currentDetails['nexus-properties']['user.name'], parsed_args['require']['nexus_os_user'])
 compareUserHome(currentDetails['nexus-properties']['user.home'], parsed_args['require']['nexus_os_user_home_dir'])
-compareMinHeapSize(parsed_args['current']['nexus_min_heap_size'],parsed_args['require']['nexus_min_heap_size'])
-compareMaxHeapSize(parsed_args['current']['nexus_max_heap_size'],parsed_args['require']['nexus_max_heap_size'])
-compareMaxDirectSize(parsed_args['current']['nexus_max_direct_memory'],parsed_args['require']['nexus_max_direct_memory'])
+compareMinHeapSize(parsed_args['current']['nexus_min_heap_size'], parsed_args['require']['nexus_min_heap_size'])
+compareMaxHeapSize(parsed_args['current']['nexus_max_heap_size'], parsed_args['require']['nexus_max_heap_size'])
+compareMaxDirectSize(parsed_args['current']['nexus_max_direct_memory'], parsed_args['require']['nexus_max_direct_memory'])
+
+def isJettyCompareRequire = compareJettyHttpsEnable(currentDetails['nexus-properties']['nexus-args'], parsed_args['require']['jetty_https_enable'])
+if ( isJettyCompareRequire ) {
+    compareJettyKeyStorePassword(parsed_args['current']['jetty_keystore_password'], parsed_args['require']['jetty_keystore_password'])
+    compareJettyKeyManagerPassword(parsed_args['current']['jetty_keymanager_password'], parsed_args['require']['jetty_keymanager_password'])
+    compareJettyTrustStorePassword(parsed_args['current']['jetty_truststore_password'], parsed_args['require']['jetty_truststore_password'])
+    compareJettyKeyStoreFile(parsed_args['current']['keystore_checksum'], parsed_args['require']['keystore_checksum'])
+}
 
 return JsonOutput.toJson(scriptResults)
-
 
 //   "nexus-configuration" : {
 //     "workingDirectory" : "/users/kube/certification_nexus_data",
 //   }
 // nexus_installation_dir: /users/kube/certification_nexus
 // nexus_data_dir: /users/kube/certification_nexus_data
-
 
 // --- httpd ----
 // httpd_config_dir
